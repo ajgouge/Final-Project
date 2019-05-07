c06426b8d4ba8208be29e3c17bb144c3fab77320
1creeperbomb <48162495+1creeperbomb@users.noreply.github.com>
2019-05-06 23:25:34 -0400

    Combination from tile system

--- Source.cpp
+++ Source.cpp
@@ -4,76 +4,190 @@
 #include <SDL_image.h>
 #include <string>
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 1920;
+const int SCREEN_HEIGHT = 1080;
 
-class Tile {
+const int TILE_SIDE = 64;
+
+void whatClicked(int x, int y, int mouse);
+
+class Tile{
 private:
-	SDL_Texture * display = NULL;
+	SDL_Renderer * renderer = NULL;
+	SDL_Texture* display = NULL;
 	int x;
 	int y;
 
 public:
+	Tile() : x(0), y(0) {}
 	Tile(int x, int y) : x(x), y(y) {}
 	~Tile();
-	bool loadTexture(char * src);
+
+	bool setTexture(const char* src);
+	void render();
+	void setX(int ix);
+	void setY(int iy);
+	int getX();
+	int getY();
+	void setRenderer(SDL_Renderer* irenderer);
+	SDL_Texture* getDisplay();
 
 };
-// lol epic
-bool init();
 
-bool loadMedia();
+SDL_Window* init(SDL_Window* window);
 
-void close();
+bool loadTexture(SDL_Renderer* renderer, SDL_Texture** tex, const char* src);
 
-void whatClicked(int x, int y, int mouse);
+void close(SDL_Window* window);
 
-int main(int argc, char * argv[])
-{
-	
-	SDL_Window * window = NULL;
-	SDL_Surface * screenSurface = NULL;
+Tile::~Tile() {
+	SDL_DestroyTexture(display);
+}
 
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+bool Tile::setTexture(const char* src) {
+	return loadTexture(renderer, &display, src);
+}
+
+void Tile::render() {
+	SDL_Rect dest = { x * TILE_SIDE, y * TILE_SIDE, TILE_SIDE, TILE_SIDE };
+	SDL_RenderCopy(renderer, display, NULL, &dest);
+}
+
+void Tile::setX(int ix) {
+	x = ix;
+}
+
+void Tile::setY(int iy) {
+	y = iy;
+}
+
+void Tile::setRenderer(SDL_Renderer* irenderer) {
+	renderer = irenderer;
+}
+
+int Tile::getX() {
+	return x;
+}
+
+int Tile::getY() {
+	return y;
+}
+
+SDL_Texture* Tile::getDisplay() {
+	return display;
+}
+
+SDL_Window* init(SDL_Window* window) {
+	SDL_DestroyWindow(window);
+	window = NULL;
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
 		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		return false;
 	}
 	else {
-		
-		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
+		window = SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (window == NULL) {
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			return NULL;
+		}
 		else {
-			
 			int imgFlags = IMG_INIT_PNG;
-			if (!(IMG_Init(imgFlags) & imgFlags))
+			if (!(IMG_Init(imgFlags) & imgFlags)) {
 				printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
-			else {
-				printf("Everything initialized properly!");
+				return NULL;
 			}
-
-			SDL_Delay(2000);
 
 		}
 	}
 
-	//pre-game loop
-	bool isRunning = true;
+	return window;
+}
 
-	int x = 0;
-	int y = 0;
+bool loadTexture(SDL_Renderer* renderer, SDL_Texture** tex, const char* src) {
+	SDL_Surface* surface = IMG_Load(src);
+	if (surface == NULL) {
+		printf("Source image for texture @ %s failed to load! SDL_image Error: %s\n", src, IMG_GetError());
+		return false;
+	}
 
-	while (isRunning) {
-		SDL_Event scanner;
+	*tex = SDL_CreateTextureFromSurface(renderer, surface);
+	if (tex == NULL) {
+		printf("Texture @ %s failed to initialize! SDL_image Error: %s\n", src, IMG_GetError());
+		return false;
+	}
+	SDL_FreeSurface(surface);
 
-		
-		while (SDL_PollEvent(&scanner)) {
+	return true;
+}
 
-			//mouse scanner
-			if (scanner.type == SDL_MOUSEBUTTONDOWN) {
-				x = scanner.button.x;
-				y = scanner.button.y;
+void close(SDL_Window* window) {
+	SDL_DestroyWindow(window);
 
-				switch (scanner.button.button) {
+	IMG_Quit();
+	SDL_Quit();
+}
+
+
+int main(int argc, char* argv[])
+{
+
+	SDL_Window* window = NULL;
+	SDL_Surface* screenSurface = NULL;
+	SDL_Renderer* renderer = NULL;
+
+	window = init(window);
+
+	if (window == NULL)
+		printf("Errors initializing\n");
+	else {
+
+		if (window == NULL)
+			printf("Ya done goofed\n");
+
+		printf("Everything initialized!\n");
+
+		Tile testImg1;
+
+		testImg1.setX(2);
+		testImg1.setY(2);
+
+		screenSurface = SDL_GetWindowSurface(window);
+		printf("Got the window surface\n");
+		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+		if (renderer == NULL) {
+			printf("Renderer could not be initialized! SDL_Error: %s\n", SDL_GetError());
+		}
+		SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+		printf("Renderer initialized\n");
+
+		testImg1.setRenderer(renderer);
+		testImg1.setTexture("assets/Mech.png");
+
+		SDL_RenderClear(renderer);
+		testImg1.render();
+		SDL_RenderPresent(renderer);
+
+		SDL_Delay(2000);
+
+		//pre-game loop
+		bool isRunning = true;
+
+		int x = 0;
+		int y = 0;
+
+		while (isRunning) {
+			SDL_Event scanner;
+
+
+			while (SDL_PollEvent(&scanner)) {
+
+				//mouse scanner
+				if (scanner.type == SDL_MOUSEBUTTONDOWN) {
+					x = scanner.button.x;
+					y = scanner.button.y;
+
+					switch (scanner.button.button) {
 					case SDL_BUTTON_LEFT:
 						std::cout << "Clicked Left! ";
 						whatClicked(x, y, 1);
@@ -86,44 +200,48 @@ int main(int argc, char * argv[])
 						std::cout << "Clicked middle! ";
 						whatClicked(x, y, 3);
 						break;
+					}
 				}
-			}
-			else if (scanner.type == SDL_KEYDOWN) {
+				else if (scanner.type == SDL_KEYDOWN) {
 
-				switch (scanner.key.keysym.sym) {
-				case SDLK_w:
-					std::cout << "W!";
-					break;
-				case SDLK_s:
-					std::cout << "S!";
-					break;
-				case SDLK_d:
-					std::cout << "D!";
-					break;
-				case SDLK_a:
-					std::cout << "A!";
-					break;
-				case SDLK_SPACE:
-					std::cout << "SPACE!";
-					break;
-				case SDLK_LSHIFT:
-					std::cout << "LEFT SHIFT!";
-					break;
-				case SDLK_LCTRL:
-					std::cout << "LEFT CTRL!";
-					break;
+					switch (scanner.key.keysym.sym) {
+					case SDLK_w:
+						std::cout << "W!";
+						break;
+					case SDLK_s:
+						std::cout << "S!";
+						break;
+					case SDLK_d:
+						std::cout << "D!";
+						break;
+					case SDLK_a:
+						std::cout << "A!";
+						break;
+					case SDLK_SPACE:
+						std::cout << "SPACE!";
+						break;
+					case SDLK_LSHIFT:
+						std::cout << "LEFT SHIFT!";
+						break;
+					case SDLK_LCTRL:
+						std::cout << "LEFT CTRL!";
+						break;
+					case SDLK_ESCAPE:
+						close(window);
+						return 0;
+						break;
+					}
 				}
 			}
+
 		}
 
+		//close
+		close(window);
+
+		return 0;
+
 	}
-
-	//close
-	SDL_DestroyWindow(window);
-	IMG_Quit();
-	SDL_Quit();
-
-	return 0;
 
 }
 
